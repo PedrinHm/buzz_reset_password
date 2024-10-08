c128a82bb1dc8d005c2c99c252f6e6b73cd36f5f
PedrinHm <pedrohm441@gmail.com>
2024-10-07 23:48:45 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve os arquivos estáticos do diretório 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Para todas as outras rotas, redireciona para a página inicial (útil para Single Page Applications)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'reset_password.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`);
+});
